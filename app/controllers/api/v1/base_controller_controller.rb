49327ac6fccea2caac9215d4fbe5667bbc8259f5
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-10-26 23:46:54 -0200

    Corrected controllers names

--- app/controllers/api/v1/base_controller_controller.rb
+++
@@ -1,3 +0,0 @@
-class Api::V1::BaseControllerController < ApplicationController
-  respond_to :json
-end
---
+++ spec/controllers/api/v1/items_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ItemsController, type: :controller do
+  describe "GET index" do
+    it "should display an array" do
+      FactoryGirl.create_list(:todo_item, 10)
+
+      get '/api/v1/items'
+
+      json = JSON.parse(response.body)
+
+      expect(response).to be_success
+
+      # expect(json['messages'].length).to eq(10)
+    end
+  end
+end
