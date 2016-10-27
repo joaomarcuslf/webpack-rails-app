5fd8c3eeca851c931091c94b3c154af57c6bd03a
João Marcus Fernandes <jmarcusfernandes@gmail.com>
2016-10-26 23:46:54 -0200

    Corrected controllers names

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'main/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :todo_item,
+                        only: [:index]
+    end
+  end
 end
