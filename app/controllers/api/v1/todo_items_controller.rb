e666828d5d18a07d46c7d091090268ebcf000980
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-10-27 23:07:23 -0200

    Added create action to TodoController

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -4,4 +4,14 @@ class Api::V1::TodoItemsController < Api::V1::BaseController
   def index
     respond_with TodoItem.all
   end
+
+  # POST api/v1/todo_items.json
+  def create
+    respond_with :api, :v1, TodoItem.create(item_params)
+  end
+
+  private
+    def item_params
+      params.require(:todo_item).permit(:id, :title, :description)
+    end
 end
