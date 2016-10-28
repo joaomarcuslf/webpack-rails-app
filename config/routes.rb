682118fc0fe5718e6829745003de48c9747fd26b
João Marcus Fernandes <jmarcusfernandes@gmail.com>
2016-10-27 22:08:54 -0200

    Changed ItemsController to TodoItemsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :todo_item,
-                        only: [:index]
+        resources :todo_items, only: [:index, :create, :destroy, :update]
+        end
     end
-  end
 end
