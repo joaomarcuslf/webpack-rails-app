112e079bf44729ef1ede6c892d79b9cc8fedfb63
João Marcus Fernandes <jmarcusfernandes@gmail.com>
2016-10-28 22:45:11 -0200

    Added CRUD actions to controller

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -10,6 +10,18 @@ class Api::V1::TodoItemsController < Api::V1::BaseController
     respond_with :api, :v1, TodoItem.create(item_params)
   end
 
+  # DELETE api/v1/todo_items.json
+  def destroy
+    respond_with TodoItem.destroy(params[:id])
+  end
+
+  # UPDATE api/v1/todo_items.json
+  def update
+    item = TodoItem.find(params[:id])
+    item.update_attributes(item_params)
+    respond_with item, json: item
+  end
+
   private
     def item_params
       params.require(:todo_item).permit(:id, :title, :description)
