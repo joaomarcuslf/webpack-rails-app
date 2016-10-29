5c086eb08be0b3928594beeadc2dbc277f80e9e9
João Marcus Fernandes <jmarcusfernandes@gmail.com>
2016-10-29 12:50:33 -0200

    Rearranjed files to use webpack

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'web_app/main'
+  root to: 'web_app#main'
+
   namespace :api do
     namespace :v1 do
         resources :todo_items, only: [:index, :create, :destroy, :update]
