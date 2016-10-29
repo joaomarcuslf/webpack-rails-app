f906380463f041b011a09463e1ddd40993fe9303
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-10-29 12:50:33 -0200

    Rearranjed files to use webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,55 @@
+// module.exports = {
+//   entry: "./app/frontend/app.es6",
+//   output: {
+//     path: "./app/assets/javascripts/",
+//     filename: "app.js"
+//   },
+//   module: {
+//     loaders: [{
+//         test: [/\.es6$/, /\.jsx$/],
+//         exclude: /node_modules/,
+//         loader: 'babel',
+//         query: {
+//             presets: ['es2015', 'react', 'stage-0']
+//         }
+//     }]
+//   }
+// };
+
+var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: "./app/frontend/app.es6",
+  context: __dirname,
+  entry: './app/frontend/app.es6',
+
   output: {
-    path: "./app/assets/javascripts/",
-    filename: "application.js"
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'application.js',
+    publicPath: '/assets',
   },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.es6'],
+    modulesDirectories: [ 'node_modules'],
+  },
+
+  plugins: [
+    new webpack.ResolverPlugin([
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('.package.json', ['main'])
+    ])
+  ],
+
   module: {
-    loaders: [{
+    loaders: [
+      {
         test: [/\.es6$/, /\.jsx$/],
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-            presets: ['es2015', 'react']
+          presets: ['es2015', 'react', 'stage-0']
         }
-    }]
+      }
+    ]
   }
 };
