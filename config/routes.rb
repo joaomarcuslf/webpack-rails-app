f0237baae4f57ea16cc3bd22669befa68b9d4644
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-10-28 22:45:11 -0200

    Added CRUD actions to controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
         resources :todo_items, only: [:index, :create, :destroy, :update]
-        end
     end
+  end
 end
