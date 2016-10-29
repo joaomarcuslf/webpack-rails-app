c0f9a7426dc425294ac7c308bfd13860280cd3f1
João Marcus Fernandes <jmarcusfernandes@gmail.com>
2016-10-29 15:31:56 -0200

    Added some plugins for startup development

---
+++ app/frontend/components/Body/Views/AllItems.jsx
@@ -0,0 +1,16 @@
+/* @flow */
+import React from 'react';
+
+export default class AllItems extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>This is AllItems Components</h1>
+      </div>
+    );
+  }
+}
