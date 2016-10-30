f3f03572290ec706cafa2bdc57b1002af3d89a87
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-10-30 10:56:15 -0200

    Added fetch API to get data and render on AllItem Component

--- app/frontend/components/Body/Views/AllItems.jsx
+++ app/frontend/components/Body/Views/AllItems.jsx
@@ -1,15 +1,53 @@
 /* @flow */
 import React from 'react';
+import 'whatwg-fetch';
 
 export default class AllItems extends React.Component {
-  constructor() {
-    super();
+  constructor(props: object) {
+    super(props);
+    this.state = { items: [] };
   }
 
-  render() {
+  getInitialState(): object {
+    return this.state;
+  }
+
+  componentDidMount() {
+    const privateThis = this;
+    fetch('/api/v1/todo_items.json', {
+      method: 'GET',
+      mode: 'cors'
+    }).then((response: object) => {
+       response.text()
+        .then((result: object) => {
+          let jsonResult = JSON.parse(result);
+          privateThis.setState({
+            items: jsonResult
+          });
+        });
+    }, (error: object) => {
+      // handle network error
+      console.error(error);
+    });
+  }
+
+  render(): ?React$Element<div> {
+    let items = this
+      .state
+      .items
+      .map((item: object): ?React$Element<div> => {
+        return(
+          <div key={item.id}>
+            <h3> {item.title} </h3>
+            <p> {item.description} </p>
+            <hr />
+          </div>
+        );
+      });
+
     return(
       <div>
-        <h1>This is AllItems Components</h1>
+        {items}
       </div>
     );
   }
