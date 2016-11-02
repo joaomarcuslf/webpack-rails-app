c4eb6247f2244382ae8de03be1736d51e87db423
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-11-02 10:40:57 -0200

    Finish NewItem component with fetch

--- app/frontend/components/Body/Views/NewItem.jsx
+++ app/frontend/components/Body/Views/NewItem.jsx
@@ -1,19 +1,65 @@
-/* @flow */
 import React from 'react';
 
 export default class NewItem extends React.Component {
-  constructor() {
-    super();
+  constructor(props: object) {
+    super(props);
+    this.state = { title: '', description: '', hasNoTitle: true };
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleTitleChange(event: object) {
+    let title = event.target.value;
+    let hasNoTitle = (title.length === 0);
+
+    this.setState({ title: title, hasNoTitle: hasNoTitle });
   }
 
-  shouldComponentUpdate(): boolean {
-    return true;
+  handleDescriptionChange(event: object) {
+    this.setState({ description: event.target.value });
+  }
+
+  handleSubmit(event: object) {
+    fetch('/api/v1/todo_items.json', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'todo_item': {
+          'title': this.state.title,
+          'description': this.state.description
+        }
+      })
+    })
+      .then((response: object) => {
+        this.setState({
+          title: '',
+          description: '',
+          hasNoTitle: true
+        });
+      });
   }
 
   render(): ?React$Element<div> {
-    return(
+    return (
       <div>
-        <h1>This is NewItem Component</h1>
+        <input type="text"
+          placeholder="Type a title."
+          value={this.state.title}
+          onChange={this.handleTitleChange} />
+        <br />
+        <textarea
+          placeholder="Type a description."
+          disabled={this.state.hasNoTitle}
+          value={this.state.description}
+          onChange={this.handleDescriptionChange}>
+        </textarea>
+        <br />
+        <button onClick={this.handleSubmit}>
+          Submit
+        </button>
       </div>
     );
   }
