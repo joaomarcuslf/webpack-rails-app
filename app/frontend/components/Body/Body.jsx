f6076605b43199cd8b01740a9397633c9de8089e
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-11-02 11:33:01 -0200

    New Refactor on components

--- app/frontend/components/Body/Body.jsx
+++ app/frontend/components/Body/Body.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import 'whatwg-fetch';
 import AllItems from './Views/AllItems.jsx';
 import NewItem from './Views/NewItem.jsx';
 
 export default class Body extends React.Component {
   constructor() {
     super();
+    this.state = { items: [] };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    const privateThis = this; // this problem
+
+    fetch('/api/v1/todo_items.json', {
+      method: 'GET',
+      mode: 'cors' // Same Origin
+    })
+      .then((response: object): object => {
+        // Fetch API will give an response with promise
+        return response.json();
+      })
+      .then((result: object) => {
+        privateThis.setState({
+          items: result
+        });
+      }, (error: object) => {
+        // handle network error
+        console.error(error);
+      });
+  }
+
+  handleSubmit(item: object) {
+    let newState = this.state.items.concat(item);
+    this.setState({ items: newState });
   }
 
   shouldComponentUpdate(): boolean {
@@ -14,9 +43,9 @@ export default class Body extends React.Component {
   render(): ?React$Element<div> {
     return(
       <div>
-        <NewItem />
+        <NewItem handleSubmit={this.handleSubmit} />
         <hr />
-        <AllItems />
+        <AllItems items={this.state.items} />
       </div>
     );
   }
