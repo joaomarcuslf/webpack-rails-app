cf96dbbe597d58fff2848d795d899df2ad77e096
João Marcus Fernandes <jmarcusfernandes@gmail.com>
2016-11-02 19:23:58 -0200

    Update some layout styles

--- app/frontend/components/App.jsx
+++ app/frontend/components/App.jsx
@@ -1,6 +1,7 @@
 /* @flow */
 import React from 'react';
 import Header from './Header/Header.jsx';
+import Menu from './Menu/Menu.jsx';
 import Body from './Body/Body.jsx';
 
 export default class App extends React.Component {
@@ -15,6 +16,7 @@ export default class App extends React.Component {
   render(): ?React$Element<div> {
     return(
       <div>
+        <Menu />
         <Header />
         <Body />
       </div>
